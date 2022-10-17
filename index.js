6ace0507ce5294903fbaa2e395dfe41b3cb75236
SDBoahen <SDBoahen@gmail.com>
2022-10-17 17:49:00 -0400

    Yall Are Doing Greatgit add .git add . :)

---
+++ index.js
@@ -0,0 +1,188 @@
+console.log( "👀" )  //
+
+
+
+
+// let someLikes = 0
+// const aConnectionToAnHTMLElement = document.querySelector( "h1" )
+
+
+
+// const someArray = []
+//     console.log( someArray )
+
+//     // someArray = [ 3, 3, 9 ]
+
+//     someArray.push( 3 )
+
+//     console.log( someArray )
+
+//     someArray.push( 400 )
+
+//     console.log( someArray )
+
+
+
+
+
+
+
+
+
+
+
+
+// const h1OnTheDOM = document.querySelector( "h1" )
+
+    // h1OnTheDOM.textContent = "<h1>:)</h1>"
+        // h1OnTheDOM.innerHTML = "<h1>:)</h1>"
+    // h1OnTheDOM.innerText = "Let's Get It! :)"
+
+    // console.log( h1OnTheDOM )
+    // h1OnTheDOM.addEventListener( "click" , ()=>{} )
+
+
+
+
+
+
+
+
+
+
+// const h1OnTheDOM = document.querySelector( "h1" )
+//     console.log( h1OnTheDOM )
+
+
+// const someObject = {
+//     // K: V 
+//     // K🚪-> V 
+//     name: "Sam",
+//     role: ""
+// }
+
+
+// h1OnTheDOM.textContent = someObject.name
+// // h1OnTheDOM.innerHTML = someObject.name
+
+
+
+// // someObject.name = "Nathan"
+//     console.log( someObject )
+
+
+
+
+
+
+
+
+
+
+// const h1OnTheDOM = document.querySelector( "h1" )
+//     console.log( h1OnTheDOM )
+
+
+// const someArray = [ 
+    
+//     // [ "Nathan" ]
+//     {
+//         name: "Nathan"
+//     }
+ 
+// ]
+
+
+// let firstObjectInArray = someArray[ 0 ]
+//     firstObjectInArray.name
+
+
+//     h1OnTheDOM.textContent = firstObjectInArray.name
+    // h1OnTheDOM.textContent = someArray[ 0 ].name
+
+
+// console.log( "We Are Trying to Get The First Index : " , someArray[ 0 ].name )
+//  Index Starts at 0  - (n-1)
+//   humanBrain :      1   2
+//                  [ {} , {} , {} ]
+//   Actual Array:     0    1    2
+
+
+
+// console.log( "Ryan's Question : " , someArray[ 0 ][ 0 ] )
+
+
+
+
+
+
+
+
+
+let objectForLeahsQuestion = {
+
+    // K:  V
+    name: "Leah",
+
+    "name-with-a-dash-do": "L-e-a-h"
+
+}
+console.log( "Leah's Question : " , objectForLeahsQuestion.name )
+//X// console.log( "Leah's Question : " , objectForLeahsQuestion.name-with-a-dash-do )
+console.log( "Leah's Question : " , objectForLeahsQuestion["name-with-a-dash-do"] )
+
+
+// let someRandomArray = [ "a" , "b" , "c" ] // n-1
+
+// someRandomArray[ 2 ]
+
+
+
+
+const maesArray = [
+
+    { name: "Nathan" }
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LOGICAL/MEMORY/INTERNAL - JS
+
+// LOGICAL/MEMORY/INTERNAL - JS
+
+
+
+
+// THE "Vesica-Pices" LOGICAL x VISUAL - JS
+
+// THE "Vesica-Pices" LOGICAL x VISUAL - JS
+
+
+
+
+// VISUAL/DOM/EVENT-HANDLING - JS
+
+
+
+// VISUAL/DOM/EVENT-HANDLING - JS
